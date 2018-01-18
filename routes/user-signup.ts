154cad8868a82bd814f5374b7a6cfb77de506daf
DmitriyLitvinov <dmutro.litvinov@gmail.com>
2018-01-18 18:03:12 +0200

    feat(following page): following page is ready for test

--- routes/user-signup.ts
+++ routes/user-signup.ts
@@ -3,9 +3,11 @@
 import * as async from 'async';
 import * as mongoose from 'mongoose';
 import { Request, Response, Express } from 'express';
-import { User } from '../models/users';
+import { User, UserResponse } from '../models/users';
+import { Event } from '../models/events';
 
 const users = mongoose.model('Users');
+const events = mongoose.model('Events');
 
 const countries = mongoose.model('Countries');
 const cities = mongoose.model('Cities');
@@ -15,6 +17,7 @@ module.exports = (app: Express): void => {
   app.get('/edit-profile/get-user-data', getUserData);
   app.post('/signup', signUpUser);
   app.post('/get-user', getUserData);
+  app.get('/get-user-followings', getUserFollowings);
   app.post('/signup/check-email', isEmailExist);
   app.post('/edit-profile/edit-user-data', editUser);
   app.post('/edit-profile/edit-user-avatar', editUserAvatar);
@@ -34,37 +37,18 @@ function getUserData(req: Request, res: Response): void | undefined {
   const query: GetUserDataInterface = req.query;
 
   if (!query.email && !query._id) {
-    res.json({success: false, data: 'user data failed', error: 'User has no email.'});
+    res.json({success: false, data: 'user data failed', error: 'User has no email.'}); // need rework resp obj
 
     return undefined;
   }
 
-  users
-    .findOne(query)
-    .lean(true)
-    .exec((err: Error, user: User): void => {
-      const newData = {
-      ...user,
-      ...{
-        statistics: {
-            followings: user.statistics.followings.length,
-            followers: user.statistics.followers.length,
-            viewers: user.statistics.viewers.length,
-            likes: {
-              likeShow: user.statistics.likes.likeShow.length,
-              likeUser: user.statistics.likes.likeUser.length,
-              liked: user.statistics.likes.liked.length
-            }
-          }
-        },
-        shows: {
-          owned: user.shows.owned.length,
-          purchased: user.shows.purchased.length
-        }
-      };
-
-      res.json({success: true, data: newData, error: err});
-    });
+  getUser({query, limit: 1})
+    .then(usr => {
+      res.json({success: true, data: usr, error: null}); // need rework resp obj
+    }).catch(err => {
+      const status = 500;
+      res.status(status).send(err);
+  });
 }
 
 function editUser(req: Request, res: Response): void | undefined {
@@ -197,4 +181,106 @@ function getCities(cb: Function): Promise<any> {
     .find({})
     .lean(true)
     .exec(cb);
+}
+
+async function getUserFollowings(req: Request, res: Response): Promise<void | undefined> {
+  const query = req.query;
+
+  if (!query.follower) {
+    const status = 500;
+    res.status(status).send({message: 'follower id is empty. Update without thats prohibited.'});
+
+    return undefined;
+  }
+
+  try {
+    const currentUser: UserResponse = await getUser({query: {_id: query.follower}, limit: 1}, false) as UserResponse;
+    const followings = currentUser.statistics.followings;
+    const usrs = await getUser({query: {_id: {$in: followings}}}) as User[];
+    const eventParams = {
+      query: {creator: {$in: followings}, datePerformance: {$gt: new Date()}},
+      sort: 'datePerformance'
+    };
+    const shows = await getEvent(eventParams) as Event[];
+    const responseObj = shows.map(show => {
+      const userIndex = usrs.findIndex(user => show.creator === user._id.toString());
+
+      return {
+        user: usrs[userIndex],
+        show
+      };
+    });
+
+    res.json(responseObj);
+  } catch (err) {
+    const status = 500;
+    res.status(status).send(err);
+  }
+}
+
+export function getUser(params: {[key: string]: any}, isTransform = true)
+  : Promise<UserResponse[] | UserResponse | User | User[]> {
+  const projection = params.projection ? params.projection : {};
+
+  return users
+    .find(params.query, projection)
+    .lean(true)
+    .limit(params.limit)
+    .exec()
+    .then((usrs: User[]) => {
+      if (!isTransform) {
+        return params.limit === 1 ? usrs[0] : usrs;
+      }
+
+      const parsedUsers =  usrs.map(user => {
+        return {
+          ...user,
+          statistics: {
+            followers: user.statistics.followers.length,
+            followings: user.statistics.followings.length,
+            viewers: user.statistics.viewers.length,
+            likes: {
+              likeShow: user.statistics.likes.likeShow.length,
+              likeUser: user.statistics.likes.likeUser.length,
+              liked: user.statistics.likes.liked.length
+            }
+          },
+          shows: {
+            owned: user.shows.owned.length,
+            purchased: user.shows.purchased.length
+          }
+        };
+      });
+
+      return params.limit === 1 ? parsedUsers[0] : parsedUsers;
+    });
+}
+
+export function getEvent(params: {[key: string]: any}, isTransform = true): Promise<Event | Event[]> {
+  const projection = params.projection ? {...params.projection, ...{buyers: false}} : {buyers: false};
+
+  return events
+    .find(params.query, projection)
+    .sort(params.sort)
+    .limit(params.limit)
+    .lean(true)
+    .exec()
+    .then((shows: Event[]) => {
+      if (!isTransform) {
+        return params.limit === 1 ? shows[0] : shows;
+      }
+
+      const parsedShows =  shows.map(show => {
+        return {
+        ...show,
+          statistics: {
+            followers: show.statistics.followers.length,
+              viewers: show.statistics.viewers.length,
+              likes: show.statistics.likes.length
+          }
+        };
+      });
+
+      return params.limit === 1 ? parsedShows[0] : parsedShows;
+    });
 }
