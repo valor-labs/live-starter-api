b72a86a45e2602c56dd46472f7a2002371f4dbb6
DmitriyLitvinov <dmutro.litvinov@gmail.com>
2018-03-06 17:54:21 +0200

    feat(nodemailer): ready for test

---
+++ config/gmail.config.ts
@@ -0,0 +1,13 @@
+interface GmailConfig {
+  USER: string;
+  CLIENT_ID: string;
+  CLIENT_SECRET: string;
+  REFRESH_TOKEN: string;
+}
+
+export const GMAIL_CONFIG: GmailConfig = {
+  USER: 'dmitriy.litvinov@valor-software.com',
+  CLIENT_ID: '333751319000-hr6oim46rint3r0sdp53hs8rb6l0n39p.apps.googleusercontent.com',
+  CLIENT_SECRET: 'Y_KC4nEIuc9B_YchbjplxtP7',
+  REFRESH_TOKEN: '1/eMc2Piigh8T0SlUv7AEifE83WCPi9opPuaNguZsAm9GxQSa9Ewt9SpgIhp0G1pW7'
+};
